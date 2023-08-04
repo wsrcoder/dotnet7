5ea255f6e926d32e223f8a6c9727ef926532dca7
wsrcoder <wsrcoder@gmail.com.br>
2023-08-04 14:44:27 -0300

    certificado para executar a aplicaçao no windows

---
+++ MauiSqLiteConnection/MauiSqLiteConnection/MauiSqLiteConnection/DeployWindows.ps1
@@ -0,0 +1,9 @@
+$certificateSubjectName = "CN=Celtica2023"
+New-SelfSignedCertificate -Type Custom -Subject $certificateSubjectName  -KeyUsage DigitalSignature -FriendlyName "Celtica - Consultoria e Serviçoes" -CertStoreLocation "Cert:\CurrentUser\My" -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.3", "2.5.29.19={text}")
+
+$cert = (Get-ChildItem cert:\CurrentUser\My   | where-object { $_.Subject -like "$certificateSubjectName" }  | Select-Object -First 1).Thumbprint
+
+$env:Path += ";C:\Program Files\Microsoft Visual Studio\2022\Preview\Msbuild\Current\Bin" 
+$env:Path += ";C:\Program Files (x86)\Windows Kits\10\App Certification Kit"
+
+msbuild /restore /t:Publish /p:TargetFramework=net7.0-windows10.0.19041.0  /p:configuration=release /p:GenerateAppxPackageOnBuild=true /p:AppxPackageSigningEnabled=true /p:PackageCertificateThumbprint=$cert
