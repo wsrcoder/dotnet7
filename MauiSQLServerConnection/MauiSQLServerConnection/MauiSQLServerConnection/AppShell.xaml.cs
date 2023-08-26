6b7973037548998261edd40776ff9efa7312feb0
wsr.coder <wsr.coder@gmail.com>
2023-08-26 16:24:15 -0300

    restruturação dos tipos de projetos

--- MauiSQLServerConnection/MauiSQLServerConnection/MauiSQLServerConnection/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MauiSQLServerConnection;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
--- MauiSqLiteConnection/MauiSqLiteConnection/MauiSqLiteConnection/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace MauiSqLiteConnection;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
